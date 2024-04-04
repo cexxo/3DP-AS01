f8557a3986ec22b0d8751f228e9f37563c3dd0af
cexxo <101983134+cexxo@users.noreply.github.com>
2024-04-04 15:33:23 +0200

    Add files via upload

---
+++ 3DP_lab_1/build/CMakeFiles/sgm.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sgm.dir/main.cpp.o"
+  "CMakeFiles/sgm.dir/main.cpp.o.d"
+  "CMakeFiles/sgm.dir/sgm.cpp.o"
+  "CMakeFiles/sgm.dir/sgm.cpp.o.d"
+  "sgm"
+  "sgm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sgm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
